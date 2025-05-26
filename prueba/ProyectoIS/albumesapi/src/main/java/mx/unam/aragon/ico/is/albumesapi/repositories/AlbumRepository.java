06cc48c3f818cd39137b1da5b4bfc378272f70ad
ArturoAzuara <arturoazuara92@aragon.unam.mx>
2025-05-25 19:56:56 -0600

    Ultimos cambios

--- prueba/ProyectoIS/albumesapi/src/main/java/mx/unam/aragon/ico/is/albumesapi/repositories/AlbumRepository.java
+++
@@ -1,8 +0,0 @@
-package mx.unam.aragon.ico.is.albumesapi.repositories;
-
-import mx.unam.aragon.ico.is.albumesapi.entities.Album;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AlbumRepository extends CrudRepository<Album, Integer> {
-
-}
